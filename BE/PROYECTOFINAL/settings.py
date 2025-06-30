3db909b5c90d523967cdc3ce81e6a8c327198866
YanitzaAstudillo <astudilloyanitza@gmail.com>
2025-06-30 07:42:35 -0600

    respon

--- BE/PROYECTOFINAL/settings.py
+++ BE/PROYECTOFINAL/settings.py
@@ -95,7 +95,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'PROYECTOBEFE',
         'USER': 'root',
-        'PASSWORD': '',
+        'PASSWORD': '1234',
         'HOST': '127.0.0.1',
         'PORT': '3306',
     }
--- FE/proyecto_front/src/components/Inicio.jsx
+++ FE/proyecto_front/src/components/Inicio.jsx
@@ -9,7 +9,7 @@ function Inicio() {
     const navigate = useNavigate()
     
     return (
-        <div className='booody'>
+        <div className='booody'style={{ backgroundImage: `url(${porta})` }}>
           
           <Link to="/PagInicio" className='enlace_sin_linea'>INICIO</Link>
           <Link to="/Nosotros" className='enlace_sin_lineaa'>NOSOTROS</Link>
@@ -17,22 +17,22 @@ function Inicio() {
           
           <div className='card-botones'>
           
-             <button id="boton1" onClick={()=>{navigate("/Farmacias")}} size="lg">Farmacias</button>
-             <button id="boton2" onClick={()=>{navigate("/Clinicas")}} size="lg">Clinicas</button>
-             <button id="boton3" onClick={()=>{navigate("/Especialidades")}} size="lg">Especialidades</button>
-             <button id="boton4" onClick={()=>{navigate("/SociosPag")}} size="lg">Afiliado</button>
-             <button id="boton5" onClick={()=>{navigate("/Mision")}} size="lg">Misión y Visión</button>
-             <button id="boton6" onClick={()=>{navigate("/Contac")}} size="lg">Contacto</button>
+             <button  onClick={()=>{navigate("/Farmacias")}} size="lg">Farmacias</button>
+             <button  onClick={()=>{navigate("/SociosPag")}} size="lg">Afiliado</button>
+             <button  onClick={()=>{navigate("/Clinicas")}} size="lg">Clinicas</button>
+             <button id= "azul" onClick={()=>{navigate("/Mision")}}>Misión y Visión</button>
+             <button  onClick={()=>{navigate("/Especialidades")}} size="lg">Especialidades</button>
+             <button  onClick={()=>{navigate("/Contac")}} size="lg">Contacto</button>
             
           </div>
-            <img src="porta" alt="porta" srcSet="../src/assets/porta.png" width={"1300"} height={738}/>
+            
             <div className="login-area">
             <p className='reg'>¿Deseas registrarte?<Link to="/Register" >Registrese aquí</Link> </p>
             <div className='tituloLogin'>
              <Link to="/Login" style={{ color: 'white', textDecoration: 'none' }} >LOGIN</Link>
              <br />
             </div>
-             <a className="nav-link" href="/Register">Deseas afiliarte?</a>
+             <a className="nav-linkk" href="/Register">Deseas afiliarte?</a>
             </div>
             
   
